595864d3143e69857c8151eb6ab2fb7971a533b4
Hitesh <hjoshi30042001@gmail.com>
2024-04-09 16:38:00 +0530

    New repository by @Hitesh

---
+++ app/src/main/java/com/example/iattendance/Sign_up_Screens/Student_signup/Student_signup_pg3.java
@@ -0,0 +1,16 @@
+package com.example.iattendance.Sign_up_Screens.Student_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.iattendance.R;
+
+public class Student_signup_pg3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_signup_pg3);
+    }
+}
